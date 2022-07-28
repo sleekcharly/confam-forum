ec9a1683ade9cc731851d67a5c5e8c9307e1a728
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-07-28 01:51:46 +0100

    updated Nav.tsx file to reuse SidebarMenus.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
-  return <div className="navigation">Nav</div>;
+  const { width } = useWindowDimensions();
+
+  const getMobileMenu = () => {
+    if (width <= 768) {
+      return (
+        <FontAwesomeIcon icon={faBars} size="lg" className="nav-mobile-menu" />
+      );
+    }
+
+    return null;
+  };
+  return (
+    <nav className="navigation">
+      {getMobileMenu()} <strong>ConfamForum</strong>
+    </nav>
+  );
 };
 
 export default Nav;
