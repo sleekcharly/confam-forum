5f2525066dfa29b2ff1ca24a12c63ce51b18590d
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-08-01 16:54:52 +0100

    set up thread component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/routes/Home";
+import Thread from "./components/routes/thread/Thread";
 
 function App() {
   const renderHome = (props: any) => <Home {...props} />;
+  const renderThread = (props: any) => <Thread {...props} />;
+
   return (
     <Switch>
       <Route exact={true} path="/" render={renderHome} />
       <Route path="/categorythreads/:categoryId" render={renderHome} />
+      <Route path="/thread/:id" render={renderThread} />
     </Switch>
   );
 }
