fe5d969e6a78748f1352805417c9b0dfe6ac30f1
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-08-11 15:36:42 +0100

    added login functionality using graphql

--- src/store/user/Reducer.ts
+++ src/store/user/Reducer.ts
@@ -1,19 +1,15 @@
+import User from "../../models/User";
+
 export const UserProfileSetType = "USER_PROFILE_SET";
-
-export interface UserProfilePayload {
-  id: string;
-  userName: string;
-}
-
 export interface UserProfileAction {
   type: string;
-  payload: UserProfilePayload | null;
+  payload: User | null;
 }
 
 export const UserProfileReducer = (
   state: any = null,
   action: UserProfileAction
-): UserProfilePayload | null => {
+): User | null => {
   switch (action.type) {
     case UserProfileSetType:
       return action.payload;
